d243801e1a70f7ce3303b25a64b4b72d6a51f6bd
Andrii Bikliann <staremi100@gmail.com>
2021-06-08 23:31:11 +0300

    first commit

---
+++ src/components/AgeSolver.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+       <span v-if="isPensioner"> Можете відпочивати</span>
+       <span v-else> Вам потрібно працювати</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : 'AgeSolver',
+        props : {
+            userAge: {
+                type : Number,
+                default : 0,
+            },
+            pensionerAge: {
+                type: Number,
+                default: 65,
+            }
+        },
+        computed : {
+            isPensioner(){
+            return this.userAge>this.pensionerAge
+            }
+        }
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
