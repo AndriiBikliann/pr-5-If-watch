8a287f4e5c83b1214680aca359e3f4033fe49680
Andrii Bikliann <staremi100@gmail.com>
2021-06-08 23:45:19 +0300

    com5

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
   <div id="app">
-    <age-solver :user-age="64"/>
+    <age-solver :user-age="age"/>
+    <age-solver-n :user-age="age"/>
+    <label>
+      Введіть вік користувача
+      <input type="number" v-model="age">
+    </label>
   </div>
 </template>
 
 <script>
 import AgeSolver from './components/AgeSolver.vue'
+import AgeSolverN from './components/AgeSolverN.vue'
 
 export default {
   name: 'App',
   components: {
-    AgeSolver
+    AgeSolver,
+    AgeSolverN,
+
+  },
+  data() {
+    return {
+      age: 21
+    }
   }
 }
 </script>
